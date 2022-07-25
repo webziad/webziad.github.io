a67500fa90b2c7fea393b0525b40d66691392538
webziad <webziad20@gmail.com>
2022-07-25 13:51:48 +0430

    class

---
+++ class/class/script.js
@@ -0,0 +1,17 @@
+function ques(){
+     document.getElementById("ques").style.display="block";
+     document.getElementById("lesson").style.display="none";
+     document.getElementById("links").style.display="none";
+}
+function lesson(){
+     document.getElementById("ques").style.display="none";
+     document.getElementById("lesson").style.display="block";
+     document.getElementById("links").style.display="none";
+
+}
+
+function ref(){
+     document.getElementById("ques").style.display="none";
+     document.getElementById("lesson").style.display="none";
+     document.getElementById("links").style.display="block";
+}
