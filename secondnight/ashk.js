1c390f15765c2989d467092ec3cc3819338c1ad7
webziad <webziad20@gmail.com>
2022-08-05 01:51:43 +0430

    delilive

--- secondnight/ashk.js
+++
@@ -1,6 +0,0 @@
-const accordion = document.getElementsByClassName('contentBox');
-for(let i=0;i<accordion.length;i++){
-     accordion[i].addEventListener('click',function(){
-          this.classList.toggle('active');
-     })
-}
