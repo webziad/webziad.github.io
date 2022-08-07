ae25def38e701c8abc90f645539f7da1bc2d51fb
webziad <webziad20@gmail.com>
2022-08-08 00:23:40 +0430

    stone

---
+++ st1/script.js
@@ -0,0 +1,219 @@
+
+function music(mp3) {
+     let audio = new Audio();
+     audio.src = mp3;
+     audio.play();
+}
+
+
+document.getElementById('fa-one').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/4fg7_part.1.mp3")
+}
+
+
+document.getElementById('fa-2').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/oeoy_part.2.mp3")
+}
+
+
+
+document.getElementById('fa-3').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/aade_part.3.mp3")
+}
+
+
+
+
+document.getElementById('fa-4').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/wnak_part.4.mp3")
+}
+
+
+document.getElementById('fa-5').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/8hjt_part.5.mp3")
+}
+
+
+document.getElementById('fa-6').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/lbbp_part.6.mp3")
+}
+
+
+document.getElementById('fa-7').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/ce4p_part.7.mp3")
+}
+
+
+document.getElementById('fa-8').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/qv9_whatsapp_audio_2022-08-07_at_7.58.45_pm.mpeg")
+}
+
+
+document.getElementById('fa-9').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/31u1_part.8.mp3")
+}
+
+
+document.getElementById('fa-10').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/6eme_part.9.mp3")
+}
+
+
+
+document.getElementById('fa-11').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/aacr_part.11.mp3")
+}
+
+
+
+document.getElementById('fa-12').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/dghq_part.12.mp3")
+}
+
+
+
+document.getElementById('fa-13').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/bofo_part.13.mp3")
+}
+
+
+
+document.getElementById('fa-14').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/zv58_part.14.mp3")
+}
+
+
+
+document.getElementById('fa-15').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/6pij_part.15.mp3")
+}
+
+
+
+document.getElementById('fa-16').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/66e8_part.16.mp3")
+}
+
+
+
+document.getElementById('fa-17').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/1nt2_part.17.mp3")
+}
+
+
+
+document.getElementById('fa-18').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/6wy8_part.18.mp3")
+}
+
+
+document.getElementById('fa-19').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/9t4_part.19.mp3")
+}
+
+
+
+document.getElementById('fa-20').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/s9c_part.20.mp3")
+}
+
+document.getElementById('fa-21').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/42rc_part.21.mp3")
+}
+
+document.getElementById('fa-22').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/e3k4_part.22.mp3")
+}
+
+
+
+document.getElementById('en-1').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/2iw9_page1.mp3")
+}
+
+document.getElementById('en-2').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/eenx_page_2.mp3")
+}
+
+document.getElementById('en-3').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/fy3q_page_3.mp3")
+}
+
+document.getElementById('en-4').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/qc1i_page_4.mp3")
+}
+
+document.getElementById('en-5').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/ije_page_5.mp3")
+}
+
+document.getElementById('en-6').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/m9jm_page_6.mp3")
+}
+
+document.getElementById('en-7').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/b1fc_page_7.mp3")
+}
+
+document.getElementById('en-8').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/cb35_page_8.mp3")
+}
+
+document.getElementById('en-9').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/61cl_page_9.mp3")
+}
+
+document.getElementById('en-10').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/q0zi_page_10.mp3")
+}
+
+
+document.getElementById('en-11').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/ip5y_page_11.mp3")
+}
+
+document.getElementById('en-12').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/8bzo_page_12.mp3")
+}
+
+document.getElementById('en-13').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/onw9_page_13.mp3")
+}
+
+document.getElementById('en-14').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/y4bf_page_14.mp3")
+}
+
+document.getElementById('en-15').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/0vcg_page_15.mp3")
+}
+
+document.getElementById('en-16').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/5sns_page_16.mp3")
+}
+
+document.getElementById('en-17').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/ry9p_18.mp3")
+}
+
+document.getElementById('en-18').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/fjia_page_19.mp3")
+}
+
+document.getElementById('en-19').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/90hb_page_20.mp3")
+}
+
+document.getElementById('en-20').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/dm8c_page_21.mp3")
+}
+
+document.getElementById('en-21').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/x62x_page_22.mp3")
+}
+
+
+document.getElementById('en-22').onclick = function () {
+     music("https://dl.pmup.ir/ftpup/1qtj_page_26.mp3")
+}
+
