77ff3a6b8ea02896ceabdfd15d1c508c583fc4c7
webziad <webziad20@gmail.com>
2022-09-11 17:21:55 +0430

    class

---
+++ class/html-file/myclass.js
@@ -0,0 +1,15 @@
+let arrow = document.getElementById('arrow');
+let lesson = document.querySelector('.lesson');
+let frame = document.querySelector('#iframe');
+let firstLesson = document.getElementsByTagName('li');
+
+firstLesson[0].addEventListener("click", () => {
+     /* اینجا محل قرار دادن لینک سورس ویدئوهای شماست */
+     frame.src = "assets/2.jpg";
+})
+
+arrow.addEventListener('click', () => {
+     lesson.classList.toggle('active');
+     frame.classList.toggle('active');
+     arrow.classList.toggle('active');
+})
